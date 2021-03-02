68e802ed745c1fa714bc3db8dc3f4b84b6a26d3e
wassef911 <1.wassef911@gmail.com>
2021-03-02 22:04:30 +0100

    refactor

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,16 +2,11 @@ import React from 'react'
 import "./homepage.style.css";
 import Registration from "../components/Registration";
 
-
 function HomePage() {
     return (
         <div className="HomePage ">
             <Registration />
         </div>
-
-
-
-
     )
 }
 
