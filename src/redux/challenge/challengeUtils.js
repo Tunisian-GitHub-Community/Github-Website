a59918d7d685d8b2ccf99d9486ebe490338b2364
wassef911 <1.wassef911@gmail.com>
2021-04-04 23:20:56 +0100

    fixed logic

--- src/redux/challenge/challengeUtils.js
+++ src/redux/challenge/challengeUtils.js
@@ -3,7 +3,7 @@ const getChallengeState = () => {
     // date is between 1 and 15. 
     if (today.getDate() <= 15) return { isOpen: true, reviewTime: false, formLink: "https://form.typeform.com/to/l8JESrOz?typeform-medium=embed-snippet" }
 
-    // date is between 16 and 25. 
+    // date is between 25. 
     if (today.getDate() >= 25) return { isOpen: false, reviewTime: true, formLink: "https://form.typeform.com/to/sVxFigO6?typeform-medium=embed-snippet" }
 
     // date is between 26 and 31. 
