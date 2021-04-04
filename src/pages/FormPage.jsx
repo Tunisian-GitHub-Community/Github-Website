89d0a7c169268296b185fed7e2347a0226756cdc
wassef911 <1.wassef911@gmail.com>
2021-04-04 22:30:27 +0100

    refactor

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -11,11 +11,11 @@ function FormPage({ challenge }) {
         script.src = "https://embed.typeform.com/embed.js";
         script.type = 'text/javascript';
         script.async = true;
-
+        console.log(challenge);
     }, [challenge])
     let render = <div>sorry</div>;
-    if (challenge.isOpen) render = <iframe title='challenge' id="typeform-full" width="100%" height="100%" frameborder="0" allow="camera; microphone; autoplay; encrypted-media;" src={challenge.formLink}></iframe>
-    if (challenge.reviewTime) render = <iframe title='challenge' id="typeform-full" width="100%" height="100%" frameborder="0" allow="camera; microphone; autoplay; encrypted-media;" src={challenge.formLink}></iframe>
+    if (challenge.isOpen) render = <iframe title='challenge' id="typeform-full" width="100%" height="100%" frameBorder="0" allow="camera; microphone; autoplay; encrypted-media;" src={challenge.formLink}></iframe>
+    if (challenge.reviewTime) render = <iframe title='challenge' id="typeform-full" width="100%" height="100%" frameBorder="0" allow="camera; microphone; autoplay; encrypted-media;" src={challenge.formLink}></iframe>
 
     return (
         <div className="form">
