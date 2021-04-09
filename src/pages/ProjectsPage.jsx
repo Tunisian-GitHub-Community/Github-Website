61816d0508a02bcb102b9492e965e576c5d017f9
wassef911 <1.wassef911@gmail.com>
2021-04-09 14:31:46 +0100

    5mins cache to req

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -4,15 +4,19 @@ import { useQuery } from 'react-query'
 import Projects from '../components/Projects';
 import Spinner from '../components/Spinner';
 
+const fetchRepos = async () => {
+  const res = await axios.get("https://api.github.com/orgs/Tunisian-GitHub-Community/repos");
+  return (res.data)
+}
 
 function ProjectsPage() {
-  const { isLoading, error, data } = useQuery('repoData', () =>
-    axios.get("https://api.github.com/orgs/Tunisian-GitHub-Community/repos").then((res) => res.data)
-  )
+  const { isLoading, error, data } = useQuery('repoData', fetchRepos, {
+    staleTime: 60 * 5000 // refresh 5 minutes
+  });
 
   if (isLoading) return <Spinner />
 
-  if (error) return 'An error has occurred'
+  if (error) return 'An error has occurred ' // TO DO : Create error component 
   return (
     <Projects projects={data} />
   );
