2de340f22ee771e84e1a1e3ce1259697847ca5c5
wassef911 <1.wassef911@gmail.com>
2021-04-11 23:30:32 +0100

    added redux middleware "thunk"

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
+import ReduxThunk from 'redux-thunk'; // no changes here 😀
 
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [ReduxThunk];
 
 export const store = createStore(
   rootReducer,
