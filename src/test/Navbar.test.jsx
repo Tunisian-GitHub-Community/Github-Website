1bb2cb3668a99ac6721b3eb7f7258abb83604d12
wassef911 <1.wassef911@gmail.com>
2021-04-15 23:55:48 +0100

    first passing test

--- src/test/Navbar.test.jsx
+++ src/test/Navbar.test.jsx
@@ -1,28 +1,21 @@
 import React from "react"
 import { shallow } from "enzyme"
-import { Provider } from "react-redux";
-import configureStore from "redux-mock-store";
-
 import { Navbar } from "../components/Navbar"
 
 
-const middlewares = []
-const mockStore = configureStore(middlewares)
-
-const store = mockStore({
+const store = {
     challenge: { isOpen: true, reviewTime: false, formLink: 'https://form.typeform.com/to/l8JESrOz?typeform-medium=embed-snippet' }
-});
+};
 let wrapper
 
 describe("Header Component", () => {
     beforeEach(() => {
-        wrapper = shallow(<Provider store={store}>
-            <Navbar />
-        </Provider>);
+        wrapper = shallow(
+            <Navbar challenge={store.challenge} />
+        );
     });
-    console.log(wrapper.debug());
     it("should render without errors", () => {
-        expect(wrapper.find("#Navbar").length).toBe(1);
+        expect(wrapper.find("[data-test='Navbar']").length).toBe(1);
     })
 
 })
