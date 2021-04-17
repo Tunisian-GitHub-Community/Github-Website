c43c5708149920b8b61fc595f0ede6b2779bb23b
wassef911 <1.wassef911@gmail.com>
2021-04-18 00:03:38 +0100

    test CI

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,6 @@
 import Enzyme from "enzyme";
 import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
-
+//test
 Enzyme.configure({
     adapter: new EnzymeAdapter(),
     disableLifecycleMethods: true // compDidMount and compDidUpdate are not called after setProps/setContext
