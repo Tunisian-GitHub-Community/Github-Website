c032fe8742fd725444d31e3da2f779f55f12e4cb
wassef911 <1.wassef911@gmail.com>
2021-04-17 01:13:32 +0100

    test suits

--- src/test/FormPage.tests/FormPage.test.jsx
+++ src/test/FormPage.tests/FormPage.test.jsx
@@ -2,30 +2,30 @@
 import React from "react"
 import { shallow } from "enzyme"
 
-import { findByTestAtrr, checkProps } from "../utils"
-import { Navbar } from "../../components/Navbar"
+import { checkProps } from "../utils"
+import { FormPage } from "../../pages/FormPage"
 
 const setUp = (props = {}) => {
-    const component = shallow(<Navbar challenge={props} />);
+    const component = shallow(<FormPage challenge={props} />);
     return component;
 };
 
 const props = { isOpen: true, reviewTime: false, formLink: 'https://form.typeform.com/to/l8JESrOz?typeform-medium=embed-snippet' }
 let wrapper;
-describe("Navbar component", () => {
+describe("FormPage component", () => {
     beforeEach(() => {
         wrapper = setUp(props)
     });
     it("should render without errors", () => {
-        expect(wrapper.find("[data-test='Navbar']").length).toBe(1);
+        expect(wrapper.find("[data-test='FormPage']").length).toBe(1);
     })
 
     it('Should not throw a props error', () => {
-        const propsErr = checkProps(Navbar, props)
+        const propsErr = checkProps(FormPage, props)
         expect(propsErr).toBeUndefined();
     });
-    it('Should render logo', () => {
-        const desc = findByTestAtrr(wrapper, 'logo');
+    it('Should render an iframe', () => {
+        const desc = wrapper.find("iframe");
         expect(desc.length).toBe(1);
     });
 })
