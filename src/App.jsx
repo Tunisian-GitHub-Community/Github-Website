f0dd60255a8d29090055c0a576589c340df819cc
wassef911 <1.wassef911@gmail.com>
2021-04-17 02:24:34 +0100

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable import/no-named-as-default */
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import Homepage from './pages/HomePage';
-
+import Router from "./Router";
 import ErrorBoundry from './components/ErrorBoundry';
 import Spinner from './components/Spinner';
 import Navbar from './components/Navbar';
@@ -11,26 +10,12 @@ import IconToOrg from './components/IconToOrg';
 import Footer from './components/Footer';
 
 
-// chuck files and laod only what is needed
-const Teampage = lazy(() => import('./pages/TeamPage'));
-const GuidelinesPage = lazy(() => import('./pages/GuidelinesPage'));
-const FormPage = lazy(() => import('./pages/FormPage'));
-const ProjectsPage = lazy(() => import('./pages/ProjectsPage'));
-const WhereToGoPage = lazy(() => import('./pages/WhereToGoPage'));
-
 export const App = () => (
   <Suspense fallback={<Spinner />}>
     <ErrorBoundry>
       <BrowserRouter>
         <Navbar />
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/team" component={Teampage} />
-          <Route exact path="/projects" component={ProjectsPage} />
-          <Route exact path="/guidelines" component={GuidelinesPage} />
-          <Route exact path="/form" component={FormPage} />
-          <Route path="*" component={WhereToGoPage} />
-        </Switch>
+        <Router />
         <IconToOrg />
         <Footer />
       </BrowserRouter>
