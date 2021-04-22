358423a7652de2295c27c912fe579ab142efd74f
wassef911 <1.wassef911@gmail.com>
2021-04-22 21:28:20 +0100

    removed devtool

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query-devtools'
 
 import App from './App';
 import { store, persistor } from './redux/store';
