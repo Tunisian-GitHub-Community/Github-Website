66070c2e8b775fc0e96d8e358c70667e530f04c2
wassef911 <1.wassef911@gmail.com>
2021-08-07 20:51:40 +0100

    more

--- src/pages/CurrentForm.jsx
+++ src/pages/CurrentForm.jsx
@@ -33,79 +33,84 @@ export const CurrentForm = () => {
   const { handleSubmit, register } = useForm();
   if (isFetching) return <Spinner />;
   return (
-    <Slider
-      {...settings}
-      ref={sliderRef}
-      className="my-20 h-4/6 w-5/6 mx-auto shadow-md sm:border-0 md:border md:border-gray-900 md:dark:border-gray-100 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100"
-    >
-      <div className="p-10 flex">
-        <div>
-          <div className="d-flex">
-            <h1 className="font-bold text-4xl mb-3">{data.intro.title}</h1>
-            <p className="font-medium text-lg mb-5">{data.intro.description}</p>
-          </div>
-          <hr className="border-gray-900 dark:border-gray-100" />
-          <div className="flex justify-center p-3">
-            <button
-              onClick={(e) => {
-                goTo(e, 1);
-              }}
-              class="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded"
-            >
-              Start
-            </button>
-          </div>
-        </div>
-      </div>
-      {formDone ? (
+    <>
+      <img src="/assets/octo.png" alt="octocat" className="flying-octo" />
+      <Slider
+        {...settings}
+        ref={sliderRef}
+        className="my-20 h-4/6 w-5/6 mx-auto shadow-md sm:border-0 md:border md:border-gray-900 md:dark:border-gray-100 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+      >
         <div className="p-10 flex">
-          <h1 className="font-bold text-4xl mb-3">{data.outro.title}</h1>
-          <p className="font-medium text-lg mb-5">{data.outro.description}</p>
-          <hr className="border-gray-900 dark:border-gray-100" />
-          <SocialMedia />
-        </div>
-      ) : (
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-col justify-center items-around p-12">
-            {data.questions.map((question, idx) => {
-              return (
-                <div className="" key={idx}>
-                  <h1 className="text-lg font-extrabold">
-                    {question.title + " "}
-                  </h1>
-                  <p className="text-md">{question.description || ""}</p>
-                  <div className="mt-1 flex shadow-md">
-                    <span className="inline-flex items-center px-3 rounded-none border border-r-0 border-gray-900 dark:border-gray-100">
-                      <i className="fas fa-user"></i>
-                    </span>
-                    <input
-                      type="text"
-                      autoComplete="off"
-                      onKeyPress={(e) => {
-                        e.key === "Enter" && e.preventDefault();
-                      }}
-                      {...register(question.id, {
-                        required: question.required,
-                      })}
-                      className="flex-1 sm:text-sm rounded-none border border-gray-900 dark:border-gray-100 bg-white dark:bg-gray-900"
-                      name={question.id}
-                    />
-                  </div>
-                </div>
-              );
-            })}
-            <div className="flex justify-center py-5 ">
+          <div>
+            <div className="d-flex">
+              <h1 className="font-bold text-4xl mb-3">{data.intro.title}</h1>
+              <p className="font-medium text-lg mb-5">
+                {data.intro.description}
+              </p>
+            </div>
+            <hr className="border-gray-900 dark:border-gray-100" />
+            <div className="flex justify-center p-3">
               <button
-                type="submit"
-                class="bg-gray-600 hover:bg-gray-500 text-white font-bold py-2 px-4 border-b-4 border-gray-800 hover:border-gray-600 rounded"
+                onClick={(e) => {
+                  goTo(e, 1);
+                }}
+                class="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded"
               >
-                Submit
+                Start
               </button>
             </div>
           </div>
-        </form>
-      )}
-    </Slider>
+        </div>
+        {formDone ? (
+          <div className="p-10 flex">
+            <h1 className="font-bold text-4xl mb-3">{data.outro.title}</h1>
+            <p className="font-medium text-lg mb-5">{data.outro.description}</p>
+            <hr className="border-gray-900 dark:border-gray-100" />
+            <SocialMedia />
+          </div>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col justify-center items-around p-12">
+              {data.questions.map((question, idx) => {
+                return (
+                  <div className="" key={idx}>
+                    <h1 className="text-lg font-extrabold">
+                      {question.title + " "}
+                    </h1>
+                    <p className="text-md">{question.description || ""}</p>
+                    <div className="mt-1 flex shadow-md">
+                      <span className="inline-flex items-center px-3 rounded-none border border-r-0 border-gray-900 dark:border-gray-100">
+                        <i className="fas fa-user"></i>
+                      </span>
+                      <input
+                        type="text"
+                        autoComplete="off"
+                        onKeyPress={(e) => {
+                          e.key === "Enter" && e.preventDefault();
+                        }}
+                        {...register(question.id, {
+                          required: question.required,
+                        })}
+                        className="flex-1 sm:text-sm rounded-none border border-gray-900 dark:border-gray-100 bg-white dark:bg-gray-900"
+                        name={question.id}
+                      />
+                    </div>
+                  </div>
+                );
+              })}
+              <div className="flex justify-center py-5 ">
+                <button
+                  type="submit"
+                  class="bg-gray-600 hover:bg-gray-500 text-white font-bold py-2 px-4 border-b-4 border-gray-800 hover:border-gray-600 rounded"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        )}
+      </Slider>
+    </>
   );
 };
 
